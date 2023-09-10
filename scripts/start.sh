766fec66faab5c95dc8730c5e9d280226c07b5b6
cyclonePORT <info@cycloneport.com>
2023-09-10 18:32:26 +0000

    2.0.10 addresses an issue on some networks with DHCP with eth0

--- scripts/start.sh
+++ scripts/start.sh
@@ -29,9 +29,9 @@ dhcpcd_config_path="/etc/dhcpcd.conf"
 DENY_ETH0="denyinterfaces eth0"
 
 if grep -q "$DENY_ETH0" "$dhcpcd_config_path" ; then
-	echo_line "Deny interface (eth0) is already set"
+	echo "Deny interface (eth0) is already set"
 else
-	echo_line "Setting denyinterface: ${DENY_ETH0}"
+	echo "Setting denyinterface: ${DENY_ETH0}"
 	
 	echo -en '\n' >> $dhcpcd_config_path
 	echo "${DENY_ETH0}" >> $dhcpcd_config_path
